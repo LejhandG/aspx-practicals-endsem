fcc05c1d451fee889b10caf0bdd38b132f37eea7
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2025-03-26 06:15:09 +0530

    Initial Commit

---
+++ Database/DatabaseUsage.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DatabaseUsage.aspx.cs" Inherits="DatabaseUsage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="ID"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Seat No"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+&nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Update" />
+&nbsp;<asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Delete" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="id" DataSourceID="SqlDataSource1">
+                <Columns>
+                    <asp:BoundField DataField="id" HeaderText="id" ReadOnly="True" SortExpression="id" />
+                    <asp:BoundField DataField="name" HeaderText="name" SortExpression="name" />
+                    <asp:BoundField DataField="seatno" HeaderText="seatno" SortExpression="seatno" />
+                </Columns>
+            </asp:GridView>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:FinalRevisionConnectionString %>" SelectCommand="SELECT * FROM [Info]"></asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Database/DatabaseUsage.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+public partial class DatabaseUsage : System.Web.UI.Page
+{
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection s1 = new SqlConnection(@"Data Source=LAPTOP-LS1B1UB8\SQLEXPRESS;Initial Catalog=FinalRevision;Integrated Security=True"))
+        {
+            string query = "INSERT INTO [info] VALUES (@id, @name, @seatno)";
+
+            using (SqlCommand sqlcmd = new SqlCommand(query, s1))
+            {
+                sqlcmd.Parameters.AddWithValue("@id", TextBox1.Text);
+                sqlcmd.Parameters.AddWithValue("@name", TextBox2.Text);
+                sqlcmd.Parameters.AddWithValue("@seatno", TextBox3.Text);
+
+                s1.Open();
+                sqlcmd.ExecuteNonQuery();
+                s1.Close();
+            }
+
+        }
+        Response.Redirect("DatabaseUsage.aspx");
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection s1 = new SqlConnection(@"Data Source=LAPTOP-LS1B1UB8\SQLEXPRESS;Initial Catalog=FinalRevision;Integrated Security=True"))
+        {
+            string query = "UPDATE [info] SET name=@name, seatno=@seatno WHERE id=@id";
+
+            using (SqlCommand sqlcmd = new SqlCommand(query, s1))
+            {
+                sqlcmd.Parameters.AddWithValue("@name", TextBox2.Text);
+                sqlcmd.Parameters.AddWithValue("@seatno", TextBox3.Text);
+                sqlcmd.Parameters.AddWithValue("@id", TextBox1.Text);
+
+                s1.Open();
+                sqlcmd.ExecuteNonQuery();
+                s1.Close();
+            }
+        }
+        Response.Redirect("DatabaseUsage.aspx");
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection s1 = new SqlConnection(@"Data Source=LAPTOP-LS1B1UB8\SQLEXPRESS;Initial Catalog=FinalRevision;Integrated Security=True"))
+        {
+            string query = "DELETE FROM [info] WHERE id=@id";
+
+            using (SqlCommand sqlcmd = new SqlCommand(query, s1))
+            {
+                sqlcmd.Parameters.AddWithValue("@id", TextBox1.Text);
+                s1.Open();
+                sqlcmd.ExecuteNonQuery();
+                s1.Close();
+            }
+        }
+        Response.Redirect("DatabaseUsage.aspx");
+    }
+}
